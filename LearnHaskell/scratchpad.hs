9825a0093db330b08ecdfe64606eb6d0f94dc754
Bhatti <bharat_saini@rediffmail.com>
2014-04-28 17:52:01 +0530

    Pattern Matching and Guards
    Pattern matching and guarding statements.

--- LearnHaskell/scratchpad.hs
+++ LearnHaskell/scratchpad.hs
@@ -2,15 +2,41 @@ module Main where
 
 main = putStrLn "Learning Haskell - ScratchPad !!"
 
-double x = x + x 
-quadruple x = double (double x)
-collection = [1,2,3,4]
-gethead = head collection
-gettail = tail collection
-take2 = take 3 collection
+getHeadOf xs = head xs
 
 myList = [a | a <- [1..10], mod (a^2) 2 == 0]
+myListOf xs = [a | a <- xs, mod (a^2) 2 == 0]
 
 lucky x = if x == 7
 			then "Lucky 7"
-			else "You Loose, Roll Again."
+			else "You Loose, Roll Again"
+
+luckyPatt :: (Integral a) => a -> String
+luckyPatt 7 = "Luck 7 !!"
+luckyPatt x = "Better Luck Next Time."
+
+myLengthList xs = sum [1 | b <- xs]
+
+myLengthPattern :: (Num y) => [x] -> y
+myLengthPattern [] = 0
+myLengthPattern (_:  xs) = 1 + myLengthPattern(xs)
+
+luckyGuard x
+	| x == 7 = "Luck 7, You Win !!"
+	| otherwise = "Not Today !!"
+
+max1 a b
+	| a > b  = a
+	| otherwise = b
+
+a `max2` b
+	| a > b = a
+	| otherwise = b
+
+initials1 firstname lastname = [f] ++ ". " ++ [l] ++ "."  
+    where 
+	(f:_) = firstname  
+	(l:_) = lastname
+
+initials2 (f:_) (l:_) = [f] ++ ". " ++ [l] ++ "."  
+
