847abae6d3f12989d6df1cbfffa4c621a3bd0baa
Bhatti <bharat_saini@rediffmail.com>
2014-05-02 12:54:16 +0530

    Adding 'take' code

--- LearnHaskell/Recursion.hs
+++ LearnHaskell/Recursion.hs
@@ -13,3 +13,11 @@ max2 [] = error "Empty List No Max."
 max2 [x] = x
 max2 (x:xs) = max x (max2 xs)
 
+take1 _ [] = []
+take1 0 _ = []
+take1 n (x:xs) = x: take1 (n-1) xs
+
+take2 n _
+	| n <= 0 = []
+take2 _ [] = []
+take2 n (x:xs) = x : take2 (n-1) xs
