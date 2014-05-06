170a47891f28410dee2b07b617e2414c2d078f97
Bhatti <bharat_saini@rediffmail.com>
2014-05-06 13:22:50 +0530

    Adding filter functions

--- LearnHaskell/3. Maps And Filters.hs
+++ LearnHaskell/3. Maps And Filters.hs
@@ -1,3 +1,14 @@
 module Main where
 
 main = putStrLn "Learning Haskell - Maps And Filters !!"
+
+map1 f [] = []
+map1 f (a:as) = f a : map1 f as
+
+filter1 _ [] = []
+filter1 p (x:xs)
+	| p x = x: filter1 p xs
+	| otherwise = filter1 p xs
+
+-- This is same as the filter function, this is to show it can be done using predicates also
+filter2 p xs = [x | x <- xs, p x == True]
