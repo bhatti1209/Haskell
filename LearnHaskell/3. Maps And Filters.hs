6e1640909ec43c3c55ab80db207fb91729db6ad1
Bhatti <bharat_saini@rediffmail.com>
2014-05-06 16:35:01 +0530

    Adding more functions.

--- LearnHaskell/3. Maps And Filters.hs
+++ LearnHaskell/3. Maps And Filters.hs
@@ -7,7 +7,7 @@ map1 f (a:as) = f a : map1 f as
 
 filter1 _ [] = []
 filter1 p (x:xs)
-	| p x = x: filter1 p xs
+	| p x = x : filter1 p xs
 	| otherwise = filter1 p xs
 
 -- This is same as the filter function, this is to show it can be done using predicates also
@@ -16,5 +16,25 @@ filter2 p xs = [x | x <- xs, p x == True]
 quicksortfilter [] = []
 quicksortfilter (x:xs) = smallerList ++ [x] ++ biggerList
 	where
-		smallerList = quicksortfilter (filter (<x) xs)
+		smallerList = quicksortfilter (filter (<=x) xs)
 		biggerList = quicksortfilter (filter (>x) xs)
+
+largestDivisble :: (Integral a) => a
+largestDivisble  = head (filter p [100000, 99999..])
+	where
+		p x = x `mod` 3829 == 0
+
+largestDiviserOfInList :: (Integral a) => a -> a -> a
+largestDiviserOfInList d x = head (filter p [x, x-1..])
+	where
+		p x = mod x d == 0
+
+takeWhile1 _ [] = []
+takeWhile1 p (x:xs)
+	| p x = x : takeWhile1 p xs
+	| otherwise = []
+
+-- sum of all odd squares less than 10000
+infiniteOddSqaures = [x^2 | x <- [1..], odd x == True]
+infiniteOddMapSquares = filter (odd) (map (^2) [1..])
+sumOfSquaresLessThan x = sum (takeWhile (<x) infiniteOddMapSquares)
