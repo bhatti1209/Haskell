1d4cff04dc7585267136374689e1f47d80aec5df
Bhatti <bharat_saini@rediffmail.com>
2014-05-06 12:12:46 +0530

    Adding few higher order functions

--- LearnHaskell/2. HigherOrderFunctions.hs
+++ LearnHaskell/2. HigherOrderFunctions.hs
@@ -2,3 +2,21 @@ module Main where
 
 main = putStrLn "Learning Haskell - Higher Order Functions !!"
 
+multiplyThreeDigits :: (Num a) => a -> a -> a -> a
+multiplyThreeDigits x y z = x * y * z
+
+multiplyTwoDigitsWithThree = multiplyThreeDigits 3
+
+divideBy10 = (/10)
+
+applyTwice f x = f (f x)
+
+zipWith1 f [] _ = []
+zipWith1 f _ [] = []
+zipWith1 f (a:as) (b:bs) = f a b : zipWith1 f as bs
+
+flip1 f a b = f b a
+
+flip2 f = g
+	where
+		g x y = f y x
