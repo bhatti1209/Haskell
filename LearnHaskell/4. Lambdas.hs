6234a61eaf359f74a3700612813a4f400bdd7c99
Bhatti <bharat_saini@rediffmail.com>
2014-05-07 13:28:35 +0530

    Adding code for Collatz sequence generator using Lambdas.

--- LearnHaskell/4. Lambdas.hs
+++ LearnHaskell/4. Lambdas.hs
@@ -2,3 +2,13 @@ module Main where
 
 main = putStrLn "Learning Haskell - Lambdas !!"
 
+chain 1 = [1]
+chain n
+	| even n = n : chain evenNumbers
+	| odd n = n : chain oddNumbers
+	where
+		evenNumbers = (n `div` 2)
+		oddNumbers = (n*3 + 1)
+
+numLongChainsLambda = length (filter (\xs -> length xs > 15) (map chain [1..100]))
+
