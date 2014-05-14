8ab61a9e90de47b2305e62c76fbfac2d3a6f6d7a
Bhatti <bharat_saini@rediffmail.com>
2014-05-14 19:03:36 +0530

    adding comment.

--- Cyber-dojo/1. 100 Doors.hs
+++ Cyber-dojo/1. 100 Doors.hs
@@ -22,4 +22,5 @@ takeFromListBy n xs = element : takeFromListBy n (drop n xs)
 	where
 		element = head (drop (n-1) xs)
 
+-- Better way to do the above
 takeFromList n xs = map fst ( [t | t <- (zip xs [1..]), (snd t) `mod` n == 0])
